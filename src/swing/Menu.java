224b5ddfb76726bcfcd178bc0b45a77c99812c24
Unknown <m.salim92@hotmail.fr>
2017-05-31 10:50:24 -0400

    A way of Using Corba

--- src/swing/Menu.java
+++ src/swing/Menu.java
@@ -1,30 +1,65 @@
 package swing;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+//import java.util.Properties;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.sql.SQLException;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
-import java.awt.Color;
-
+/*
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import PersonnelAPP.PersonnelHelper;
+import PersonnelAPP.Personnel;
+//*/
 public class Menu extends JFrame {
 
 	private JPanel contentPane;
+	//private static Personnel personnelImpl;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		/* REference in Naming
+		try{
+			// create and initialize the ORB
+			Properties props = new Properties();
+			props.put("org.omg.CORBA.ORBInitialPort", "1000");
+			props.put("org.omg.CORBA.ORBInitialHost", "192.168.0.187");
+			ORB orb = ORB.init(args, props);
+			// get the root naming context
+			org.omg.CORBA.Object objRef = 
+					orb.resolve_initial_references("NameService");
+			// Use NamingContextExt instead of NamingContext. This is 
+			// part of the Interoperable naming Service.  
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+			// resolve the Object Reference in Naming
+			String name = "Personnel";
+			personnelImpl = PersonnelHelper.narrow(ncRef.resolve_str(name));
+
+			System.out.println("Obtained a handle on server object: " + personnelImpl);
+			personnelImpl.AfficherEtudiants();
+
+			//personnelImpl.shutdown();
+
+		} catch (Exception e) {
+			System.out.println("ERROR : " + e) ;
+			e.printStackTrace(System.out);
+		}
+		//*/
+
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -47,20 +82,20 @@ public class Menu extends JFrame {
 		contentPane.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Menu Principal", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 434, 43);
 		contentPane.add(menuBar);
-		
+
 		JMenu mnFichier = new JMenu("Fichier");
 		menuBar.add(mnFichier);
-		
+
 		JMenu mnNouveau = new JMenu("Nouveau");
 		mnFichier.add(mnNouveau);
-		
+
 		JMenuItem mntmEnseignant = new JMenuItem("Enseignant");
 		mnNouveau.add(mntmEnseignant);
-		
+
 		JMenuItem mntmEtudiant = new JMenuItem("Etudiant");
 		mnNouveau.add(mntmEtudiant);
 		mntmEtudiant.addActionListener(new ActionListener() {
