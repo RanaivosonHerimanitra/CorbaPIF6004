2c33d234aa65febaaddb3c136b23ad410a3f98bf
Herimanitra Ranaivoson <ranaivos@MATINF.uqtr.ca>
2017-06-01 13:30:59 -0400

    delete all swing

--- src/swing/FormListener.java
+++
@@ -1,9 +0,0 @@
-package swing;
-
-import java.sql.SQLException;
-import java.util.EventListener;
-
-public interface FormListener extends EventListener{
-	public void formEventOccured(FormEventEnseignat e) throws SQLException;
-
-}
--- src/swing/FormListenerEtudiant.java
+++
@@ -1,7 +0,0 @@
-package swing;
-
-import java.util.EventListener;
-
-public interface FormListenerEtudiant extends EventListener{
-	public void formEventOccuredAddEtudiant(FormEventEtudiant e);
-}
