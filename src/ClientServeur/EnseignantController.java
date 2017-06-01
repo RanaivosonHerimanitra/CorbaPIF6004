d7f2d28d42bbb556d5943ea20c3dc03c7a1dcda5
Unknown <m.salim92@hotmail.fr>
2017-06-01 14:56:41 -0400

    enseignant controller update

--- src/ClientServeur/EnseignantController.java
+++ src/ClientServeur/EnseignantController.java
@@ -11,56 +11,96 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import PersonnelAPP.Enseignant;
-import PersonnelAPP.Etudiant;
 import PersonnelAPP.PersonInfo;
 
 public class EnseignantController {
+	//private static  LinkedList<Enseignant> ListEnseignant ;//today
 	private Connection conn;
-	
+
 	public EnseignantController()  {
 		conn = ConnectionDB.getConnection();
+		//ListEnseignant = new LinkedList<Enseignant>();//today
 	}
-	public ArrayList<Enseignant> getEnseignant() throws SQLException 
-	{
+
+
+	public ArrayList<Enseignant> getEnseignant() throws SQLException{
 		String sql = "SELECT * FROM enseignant";
 		Statement stmt = conn.createStatement();
 		ResultSet rs= stmt.executeQuery(sql);
 		ArrayList<Enseignant> ListEnseignant = new ArrayList<Enseignant>();
 
 		while (rs.next()){
-			String nom = rs.getString("nom_ens");
-		    String prenom = rs.getString("prenom_ens");
-		    String domaine = rs.getString("domaine_act_ens");
-		    long tel_bureau = rs.getLong("tel_bureau");
-		    long numero_poste = rs.getLong("numero_poste");
-		    String courriel_ens = rs.getString("courriel_ens");
-		    Enseignant e = new Enseignant(new PersonInfo(nom,prenom,courriel_ens,domaine),tel_bureau,numero_poste) ;
-			ListEnseignant.add(e);
+			ListEnseignant.add(new Enseignant(new PersonInfo(rs.getString("nom_et"),rs.getString("prenom_et"),
+					rs.getString("courriel_et"),rs.getString("domaine_act_et")),rs.getLong("tel_bureau"),
+					rs.getLong("numero_poste")));
 		}
 		return ListEnseignant;
-	
+		/*
+		String sql = "SELECT * FROM enseignant";
+		//today
+		ArrayList<Enseignant> ListEnseignant = new ArrayList<Enseignant>();
+		//List<Enseignant> ListEnseignant = new LinkedList<Enseignant>();
+		//today fin
+		Statement statement = conn.createStatement();
+		ResultSet result = statement.executeQuery(sql);
+		while (result.next()){
+		    String nom = result.getString("nom_ens");
+		    String prenom = result.getString("prenom_ens");
+		    String domaine = result.getString("domaine_act_ens");
+		    long tel_bureau = result.getLong("tel_bureau");
+		    long numero_poste = result.getLong("numero_poste");
+		    String courriel_ens = result.getString("courriel_ens");
+		    Enseignant e = new Enseignant(new PersonInfo(nom,prenom,courriel_ens,domaine),tel_bureau,numero_poste) ;
+
+		    ListEnseignant.add(e);
+		}
+		//today 
+
+		System.out.println("la");
+		return ListEnseignant;
+		//return (ArrayList<Enseignant>) Collections.unmodifiableList(ListEnseignant);
+		//today
+		 */
+
 	}
+	//today debut
+	public void removeEnseignant(int index){
+		try {
+			getEnseignant().remove(index);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//today fin
 	public Enseignant selectEnseignant(String nom, String prenom) throws SQLException{
 		String sql = "SELECT * FROM enseignant WHERE nom_ens=? AND prenom_ens=?";
-		Enseignant e = null;
-		PreparedStatement statement = conn.prepareStatement(sql);
-		statement.setString(1, nom);
-		statement.setString(2, prenom);
-		 
-		ResultSet result= statement.executeQuery();
-		while ( result.next() ) {
-			if (result.getString("nom_ens").equals(nom)){
-				 e = new Enseignant(new PersonInfo(result.getString("nom_ens"),result.getString("prenom_ens"),result.getString("courriel_ens"),result.getString("domaine_act_ens")),result.getInt("tel_bureau"),result.getInt("numero_poste")) ;
-				 System.out.println(e.p.nom+" is found");
+		PreparedStatement statement;
+		try {
+			statement = conn.prepareStatement(sql);
+			statement.setString(1, nom);
+			statement.setString(2, prenom);
+			System.out.println("before search");
+			ResultSet result;
+			System.out.println("before query");
+			result = statement.executeQuery();
+			System.out.println("After query");
+			while (result.next()){
+				Enseignant e = new Enseignant(new PersonInfo(result.getString("nom_ens"),result.getString("prenom_ens"),result.getString("courriel_ens"),result.getString("domaine_act_ens")),result.getInt("tel_bureau"),result.getInt("numero_poste")) ;
+				System.out.println(e.p.nom+" is found");
+				return e;
 			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			//e1.printStackTrace();
+			return new Enseignant(new PersonInfo("","","",""),0,0);
 		}
-		System.out.println(e);
-		return e;
-		//return null;
+		return new Enseignant(new PersonInfo("","","",""),0,0);
 	}
+
 	public void insertion(Enseignant e) throws SQLException {
 		String sql = "INSERT INTO enseignant (nom_ens, prenom_ens, domaine_act_ens, tel_bureau, numero_poste, courriel_ens) VALUES (?, ?, ?, ?,?,?)";
-		
+
 		PreparedStatement statement = conn.prepareStatement(sql);
 		statement.setString(1, e.p.nom);
 		statement.setString(2, e.p.prenom);
@@ -71,12 +111,12 @@ public class EnseignantController {
 		System.out.println(statement);
 		int rowsInserted = statement.executeUpdate();
 		if (rowsInserted > 0) {
-		    System.out.println("A new user was inserted successfully!");
+			System.out.println("A new user was inserted successfully!");
 		}
 	}
 	public void update(Enseignant oldE, Enseignant modifE) throws SQLException {
 		String sql = "UPDATE enseignant SET nom_ens=?, prenom_ens=?, domaine_act_ens=?, tel_bureau=?, numero_poste=?, courriel_ens=? WHERE nom_ens=?";
-		 
+
 		PreparedStatement statement = conn.prepareStatement(sql);
 		statement.setString(1, oldE.p.nom);
 		statement.setString(2, oldE.p.prenom);
@@ -85,21 +125,22 @@ public class EnseignantController {
 		statement.setLong(5, oldE.post);
 		statement.setString(6, oldE.p.mail);
 		statement.setString(7, modifE.p.nom);
-		 
+
 		int rowsUpdated = statement.executeUpdate();
 		if (rowsUpdated > 0) {
-		    System.out.println("An existing user was updated successfully!");
+			System.out.println("An existing user was updated successfully!");
 		}
 	}
+
 	public void delete(Enseignant e) throws SQLException 
 	{
 		String sql = "DELETE FROM enseignant WHERE nom_ens=?";
 		PreparedStatement statement = conn.prepareStatement(sql);
 		statement.setString(1, e.p.nom);
-		 
+
 		int rowsDeleted = statement.executeUpdate();
 		if (rowsDeleted > 0) {
-		    System.out.println("A user was deleted successfully!");
+			System.out.println("A user was deleted successfully!");
 		}
 	}
 
